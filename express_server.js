7aa66511c2d2b03c95284fa1880db12ff1b060fd
kmiecik013 <kmiecik013@gmail.com>
2021-12-07 07:30:14 +0000

    added new urls( submit form

--- express_server.js
+++ express_server.js
@@ -2,12 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+
+};
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/hello", (req,res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+})
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "x5672": "https://www.ign.com/ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -17,13 +32,15 @@ app.get("/urls.json", (reg,res) => {
   res.json(urlDatabase);
 })
 
-app.get("/hello", (req,res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+
 
 app.get("/urls",(req, res) => {
   const templateVars = {urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -37,3 +54,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("I hope this works");         // Respond with 'Ok' (we will replace this)
+});
