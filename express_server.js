b4f32c73e9ddf7124051af3263b13f240f2bd689
kmiecik013 <kmiecik013@gmail.com>
2021-12-07 06:16:29 +0000

    update on express Server

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  "x5672": "https://www.ign.com/ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -21,6 +21,19 @@ app.get("/hello", (req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
 
+app.get("/urls",(req, res) => {
+  const templateVars = {urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase};
+  res.render("urls_show", templateVars);
+  console.log("ign.com")
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
