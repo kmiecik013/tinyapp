048e78072c3483fa3290148eb2936c12e6a2d337
kmiecik013 <kmiecik013@gmail.com>
2021-12-08 00:09:23 +0000

    done part 1

--- express_server.js
+++ express_server.js
@@ -1,21 +1,20 @@
-const express = require("express");
+const bodyParser = require("body-parser");
+const express = require("express")
+
 const app = express();
 const PORT = 8080; // default port 8080
 
-function generateRandomString() {
-
-};
 
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("view engine", "ejs");
 
 app.get("/hello", (req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
 
-app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "x5672": "https://www.ign.com/ca",
@@ -44,9 +43,9 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
-  console.log("ign.com")
+ 
 });
 
 
@@ -55,6 +54,35 @@ app.listen(PORT, () => {
 });
 
 app.post("/urls", (req, res) => {
+  const newString = generateRandomString()
+  const longURL = req.body.longURL;
+   
+  urlDatabase[`${newString}`] = `${longURL}` 
+  res.redirect(`/urls/${newString}`)  
   console.log(req.body);  // Log the POST request body to the console
-  res.send("I hope this works");         // Respond with 'Ok' (we will replace this)
+  //res.send("Ok");  
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL]
+  // const longURL = ...
+  res.redirect(longURL);
+});
+/*
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL); //res stands for response
+});
+
+*/
+// create a shortURL
+// key = short, value = longurl
+// use redirect 
+
+function generateRandomString() {
+  
+    const result = Math.random().toString(36).substring(2,7);
+    return  result
+};
