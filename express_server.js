b0acb889f924fe83be11e958929dbd299d8d3d8a
kmiecik013 <kmiecik013@gmail.com>
2021-12-10 04:51:48 +0000

    finally done part of this

--- express_server.js
+++ express_server.js
@@ -1,11 +1,11 @@
 const bodyParser = require("body-parser");
 const { render } = require("ejs");
 const express = require("express")
+const cookieParser = require('cookie-parser');
 
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; 
 
-const cookieParser = require('cookie-parser');
 
 app.use(cookieParser())
 
@@ -13,17 +13,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-app.get("/hello", (req,res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
 
+const users ={
+  "userRandomID":{
+    id: "userRandomID" ,
+    email: "user@example.com",
+    password: "sure"
+  },
 
+  "userRandomID2" :{
+    id: "userRandomID2",
+    email: "user2@example.com",
+    password : "yes2"
+  }
+}
+const emailchecker = function(email){
+  for(let id in users) {
+    if(users[id].email === email) {
+      return users[id]
+    }
+  } 
+  return false
+}   
 
 const urlDatabase = {
   "x5672": "https://www.ign.com/ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/hello", (req,res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+})
 
 
 app.get("/", (req, res) => {
@@ -36,13 +56,16 @@ app.get("/urls.json", (reg,res) => {
 
 
 app.get("/urls",(req, res) => {
-  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
+  const templateVars = {urls: urlDatabase, 
+    user: users[req.cookies["user_id"]]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
-  res.render("urls_new");
+  const templateVars = {urls: urlDatabase, 
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("urls_new",templateVars);
 });
 /*
 app.get("/urls/:shortURL", (req, res) => {
@@ -54,7 +77,8 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/urls/:shortURL", (req,res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[req.params.shortURL]
-  const templateVars ={ shortURL, longURL, }
+  const templateVars ={ shortURL, longURL,
+    user: req.cookies["user_id"] }
  
   res.render("urls_show", templateVars);
 
@@ -93,28 +117,101 @@ app.post("/urls/:id", (req,res) => {
   const shortURL = req.params.id;
   const alphaLongULR = req.body.longURL;
   urlDatabase[shortURL] = alphaLongULR
-//console.log(shortURL);
 
-//console.log(req.body)
-//console.log(alphaLongULR);
-
-
-  console.log(urlDatabase);
+  //console.log(urlDatabase);
   res.redirect("/urls");
 });
 
+
 app.post("/login", (req,res) => {
 
-  const username = req.body.username
-  res.cookie('username', username)
+
+    //const email =req.body.email
+    //const password = req.body.password
+
+    const alphaUser = emailchecker(req.body.email);
+
+    if (!req.body.email || !req.body.password) {
+      return res.status(403).send("email and password cannot be blank");
+    }
+  
+    if (!alphaUser) {
+      return res.status(403).send("User with that Username  exists, please regist");
+    }
+  
+    if (alphaUser.password !== req.body.password) {
+      return res.status(403).send('password does not match');
+    }
+console.log("test", alphaUser);
+  
+  res.cookie("user_id", alphaUser.id);
   res.redirect("urls")  
 
 })
 
+
 app.post("/logout",(req,res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('urls');
 });
+
+app.get("/register", (req,res) => {
+  const templateVars = {urls: urlDatabase, 
+    user: users[req.cookies["user_id"]]};
+
+res.render("urls_register", templateVars)
+
+});
+
+app.get("/login", (req,res) => {
+  const templateVars = {urls: urlDatabase, 
+    user: users[req.cookies["user_id"]]}
+  
+    res.render("urls_login", templateVars)
+
+})
+
+
+
+
+
+
+app.post("/register", (req,res) => {
+
+const userID = generateRandomString()
+
+ 
+
+if (!req.body.email || !req.body.password) {
+  return res.status(400).send("STATUS ERROR ; an email and password cannot be blank");
+}
+
+const alphaUser = emailchecker(req.body.email);
+
+  if (alphaUser) {
+    return res.status(400).send("Username/Email already taken");
+  }
+
+users[userID] = {
+  
+    id: userID,
+    email: req.body.email,
+    password: req.body.password,
+}
+  //const stringifyuser = JSON.stringify(user);
+  //users[user.id] = user;
+
+  
+  res.cookie("user_id", userID);
+  
+  console.log(users)
+  //console.log(user)
+  res.redirect(`/urls`);
+})
+
+
+
+
 
 /*
 app.post("urls/:shortURL",(req,res) => {
@@ -125,6 +222,7 @@ app.post("urls/:shortURL",(req,res) => {
 });
  
 */
+
 
 
 
@@ -153,3 +251,8 @@ function generateRandomString() {
 
 
 //clear cookie = 
+
+
+//changed the POST in the register form by adding users to an object
+//identied the route from GET post was gioing to login 
+//was going to regiester = cannot post = issue with routes
